6042cb876f1b793ab886495beb6b6e39fb07d7ef
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-01-05 20:35:39 +0100

    Process global: the console arguments

---
+++ basic/globals/process/README.md
@@ -0,0 +1,4 @@
+Explanation of the process global
+=================================
+
+- argv.js: it explains how the console arguments are returned in node.js
---
+++ basic/globals/process/argv.js
@@ -0,0 +1,6 @@
+// You can retrieve the console arguments using process.argv
+// It returns an array:
+// First element is node
+// Second element is the file running in Node
+// All the other elements are the console arguments
+console.log(process.argv);
